c6d205bee392ca3796d0f40909b5422d25cca899
André Hopf <andre.hopf@gmail.com>
2022-11-09 12:58:36 -0300

    exercicio 6 - fetch

---
+++ exercicio6/app.js
@@ -0,0 +1,22 @@
+const procuraIdadeMediaNoBrasil = async nome => {
+  try {
+    const nomeEmLowerCase = nome.toLowerCase();
+    const response = await fetch(
+      `https://api.agify.io/?country_id=BR&name=${nomeEmLowerCase}`
+    );
+    const resultado = await response.json();
+
+    const nomeCapitalized =
+      nomeEmLowerCase[0].toUpperCase() + nomeEmLowerCase.slice(1);
+
+    console.log(
+      `A idade média no Brasil das pessoas com o nome de ${nomeCapitalized} é de ${resultado.age}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+procuraIdadeMediaNoBrasil('André');
+procuraIdadeMediaNoBrasil('rachel');
+procuraIdadeMediaNoBrasil('luiz');
