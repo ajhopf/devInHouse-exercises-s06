66c14b5d1e1611bd847781a29972029899af661a
André Hopf <andre.hopf@gmail.com>
2022-11-11 19:08:35 -0300

    atualiza ex1

--- exercicio1/app.js
+++ exercicio1/app.js
@@ -1,9 +1,7 @@
 const sleep = valor => {
   return new Promise((res, rej) => {
-    setTimeout(() => console.log(valor), 3000);
+    setTimeout(() => res(valor), 3000);
   });
 };
 
-sleep('olá');
-sleep(200);
-sleep(true);
+sleep('olá').then(resultado => console.log(resultado));
