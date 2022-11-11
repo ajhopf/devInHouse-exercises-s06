675d5604ffaccf9d48bbb46843c6410df81c6f08
André Hopf <andre.hopf@gmail.com>
2022-11-11 19:37:39 -0300

    utiliza toLocaleTimeString()

--- exercicio2/app.js
+++ exercicio2/app.js
@@ -3,14 +3,16 @@ const divRelogio = document.querySelector('.relogio');
 const atualizaHora = () => {
   const horarioAtual = new Date();
 
-  const horaAtual = horarioAtual.getHours();
-  const minutoAtual = horarioAtual.getMinutes();
-  const segundoAtual = horarioAtual.getSeconds();
+  // const horaAtual = horarioAtual.getHours();
+  // const minutoAtual = horarioAtual.getMinutes();
+  // const segundoAtual = horarioAtual.getSeconds();
 
-  divRelogio.innerHTML = '';
-  divRelogio.innerHTML = `${horaAtual < 10 ? '0' + horaAtual : horaAtual}:${
-    minutoAtual < 10 ? '0' + minutoAtual : minutoAtual
-  }:${segundoAtual < 10 ? '0' + segundoAtual : segundoAtual}`;
+  // divRelogio.innerHTML = '';
+  // divRelogio.innerHTML = `${horaAtual < 10 ? '0' + horaAtual : horaAtual}:${
+  //   minutoAtual < 10 ? '0' + minutoAtual : minutoAtual
+  // }:${segundoAtual < 10 ? '0' + segundoAtual : segundoAtual}`;
+
+  divRelogio.innerHTML = horarioAtual.toLocaleTimeString();
 };
 
 atualizaHora();
